34fa96f62768cc1bfc16a6d2e6fb53728f1dd11b
rotaliano <rotaliano@gmail.com>
2019-10-03 23:03:43 +0900

    commit

--- src/guice/BillingService.java
+++ src/guice/BillingService.java
@@ -1,16 +1,18 @@
 package guice;
 
 import com.google.inject.Inject;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor(onConstructor = @__({@Inject}))
 public class BillingService {
     private final CreditCardProcessor processor;
     private final TransactionLog transactionLog;
 
-    @Inject
-    public BillingService(CreditCardProcessor processor, TransactionLog transactionLog) {
-        this.processor = processor;
-        this.transactionLog = transactionLog;
-    }
+//    @Inject
+//    public BillingService(CreditCardProcessor processor, TransactionLog transactionLog) {
+//        this.processor = processor;
+//        this.transactionLog = transactionLog;
+//    }
 
     public Receipt changeOrder(PizzaOrder order, CreditCard creditCard) {
         System.out.println("new Receipt");
