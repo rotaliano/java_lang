d06d0ef39bf57092ccc18900dbaa6e484040bade
stakahashi-gladd <stakahashi@labellevie.jp>
2019-10-18 19:38:52 +0900

    commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package main.java;
+
+import org.apache.commons.lang3.SystemUtils;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println(SystemUtils.JAVA_VM_VERSION);
+    }
+}
