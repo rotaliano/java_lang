09b3d3b52010cd2a4f8dd53bceaf247e739aca86
rotaliano <rotaliano@gmail.com>
2019-11-28 22:44:13 +0900

    commit

--- src/uuid/UuidTest.java
+++ src/uuid/UuidTest.java
@@ -1,14 +1,20 @@
 package uuid;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 public class UuidTest {
     public static void main(String[] args) {
-        UUID uuid = UUID.randomUUID();
-        String str = uuid.toString();
-        System.out.println(str);
-        System.out.println(str.length());
+        List<UUID> uuids = new ArrayList<>();
+        for (int i = 0; i <10; i++) {
+            uuids.add(UUID.randomUUID());
+        }
+
+        uuids.stream().forEach(x -> {
+            System.out.println(x);
+            System.out.println(x.toString().length());
+        });
     }
 }
